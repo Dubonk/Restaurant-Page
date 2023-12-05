4abb8a471e39134620bca8c778d27eecb539aa77
Kevin <kevindubon43@gmail.com>
2023-12-04 16:10:48 -0800

    Done with home tab, still need menu and contact.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 import './style.css';
+import './styles/home.css'
+import './scripts/home';
+import githubImg from './svgs/github.svg';
+import renderHome from './scripts/home';
 
-console.log('hello world');
+const createLinks = (function () {
+    const links = document.createElement('div');
+    links.classList.add('links');
+    const home = document.createElement('button');
+    home.innerText = 'Home';
+    const menu = document.createElement('button');
+    menu.innerText = 'Menu';
+    const contact = document.createElement('button');
+    contact.innerText = 'Contact';
+    
+    links.appendChild(home);
+    links.appendChild(menu);
+    links.appendChild(contact);
+    
+    document.body.prepend(links);
+})();
+
+const createTitle = (function () {
+    const title = document.createElement('div');
+    title.setAttribute('id', 'title');
+    const h1 = document.createElement('h1');
+    h1.innerText = "Koni's Place";
+    title.appendChild(h1);
+    
+    document.body.prepend(title);
+})();
+
+const createFooter = (function () {
+    const footer = document.createElement('footer');
+    const github = new Image();
+    github.src = githubImg;
+    github.setAttribute('id', 'githubImg');
+    footer.appendChild(github);
+    document.body.appendChild(footer);
+})();
+
+renderHome();
---
+++ src/scripts/contact.js
@@ -0,0 +1,6 @@
+const content = document.getElementById('content');
+
+const renderContact = (function () {
+    let contactNumber = '(555)-321-8888';
+    content.appendChild(contactNumber);
+});
